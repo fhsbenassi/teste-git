d564336d62160dc07ae4438352d1cc42c1fcfe0a
fhsbenassi <fhsbenassi@gmail.com>
2020-09-09 16:44:54 -0300

    testando commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,5 +7,8 @@ public class Program {
 		System.out.println("Bom dia!");
 		System.out.println("Boa tarde!");
 		System.out.println("Boa noite!");
+		System.out.println("Boa noite!");
+		System.out.println("Boa noite!");
+		System.out.println("Boa noite!");
 	}
 }
